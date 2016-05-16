39162c4d6110154df8cb5e1e3d197d2cb8fe9894
Eva <Bresciano>
2016-05-16 16:42:06 -0600

    Progress on About ME

---
+++ AboutMe2.0/AboutMe2.0/AboutMeViewController.swift
@@ -0,0 +1,53 @@
+//
+//  AboutMeViewController.swift
+//  AboutMe2.0
+//
+//  Created by Eva Marie Bresciano on 5/16/16.
+//  Copyright © 2016 Module 6.2. All rights reserved.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Actions 
+    
+    @IBAction func foodToDetailView(sender: AnyObject) {
+    }
+    
+    @IBAction func musicToDetailView(sender: AnyObject) {
+    }
+    
+    @IBAction func moviesToDetailView(sender: AnyObject) {
+    }
+    
+    @IBAction func sportsToDetailView(sender: AnyObject) {
+    }
+   
+    @IBAction func familyToDetailView(sender: AnyObject) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AboutMe2.0/AboutMe2.0/FunFactsDetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FunFactsDetailViewController.swift
+//  AboutMe2.0
+//
+//  Created by Eva Marie Bresciano on 5/16/16.
+//  Copyright © 2016 Module 6.2. All rights reserved.
+//
+
+import UIKit
+
+class FunFactsDetailViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var detailImages: UIImageView!
+    
+    @IBOutlet weak var detailText: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK:-> Actions
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
