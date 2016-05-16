521b92cd57cf171982a6cea14415b4a59b632b6b
Eva <Bresciano>
2016-05-16 09:31:00 -0600

    stretch

---
+++ Stretch Problem 1.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+class Person {
+    
+   let name: String?
+   let favColor: String?
+   let favMovie: String?
+    
+    init(name: String?, favColor: String?, favMovie: String?){
+        self.name = name
+        self.favColor = favColor
+        self.favMovie = favMovie
+       
+    }
+
+}
+
+
+/*struct somePerson {
+    var name: String
+    var favMovie: String
+    
+    init(name: String, favMovie: String){
+        self.name = "Eva"
+        self.favMovie = "the Godfather"
+    } */
+
+
+
+
+
